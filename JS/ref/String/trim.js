e6a50666d4f9d2b17f31581d2182b7947d8eadc6
lanhong <bskool@yeah.net>
2013-12-27 10:09:10 +0800

    String trim

---
+++ JS/ref/String/trim.js
@@ -0,0 +1,17 @@
+var fooStr = " foo ";
+
+var result = fooStr.trim();
+
+console.log(result);
+
+/** 
+ * 兼容ie
+ * @note: https://developer.mozilla.org/zh-CN/docs/JavaScript/Reference/Global_Objects/String/Trim
+ */
+
+if(!String.prototype.trim){
+    String.prototype.trim = function(){
+        return this.replace(/^\s+|\s+$/g,'');
+    }
+}
+
