82c080050970b0c18b42e3ae03ac9cd067a7d656
hidoos <sk8huaban@gmail.com>
2013-12-27 15:00:51 +0800

    静态调用json

--- JS/ajax/index.js
+++ JS/ajax/index.js
@@ -1,14 +1,27 @@
 /**
  * showPrice
  * @param {object} JSON.parse || $.JSON.prase
- * @return {}
- * */
-function showPrice (data){
-    console.log("ticker symbol:",data.symbol);
-    console.log("\rticker price:",data.price);
-}
+ * @return {obj}
+ * @doc: 这个就是单列子模式
+ *
+ */
+var init = (function() {
 
+    function showPrice(data) {
+        console.log("ticker symbol:", data.symbol);
+        console.log("\rticker price:", data.price);
+    }
 
-showPrice(ticker);
+    /* static invoketed */
+    var tickerUrl = 'ticker.js';
 
+    var srcEl = document.createElement('script');
+    srcEl.setAttribute('src', tickerUrl)
 
+    document.getElementsByTagName('head')[0].appendChild(srcEl);
+    
+    return {
+        show : showPrice
+    }
+
+})();
