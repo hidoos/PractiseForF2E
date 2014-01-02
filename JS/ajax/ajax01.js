3da682fdb4e10db7c7423e2e85e8f149994803bb
hidoos <sk8huaban@gmail.com>
2014-01-02 11:01:06 +0800

    create ajax

--- JS/ajax/ajax01.js
+++ JS/ajax/ajax01.js
@@ -1,23 +1,31 @@
 /**
- * 先创建一个xhr对象
- */
-
-
-/**
+ * 通过 XMLHttpRequest 创建一个xhr对象
  * @param {object}
  * @return {object}
  */
 function createXhr(obj) {
-  var obj = null;
+    var obj = null;
 
+    if (window.XMLHttpRequest) {
+        obj = new XMLHttpRequest();
+    } else {
+        try {
+            obj = new XMLHTTP('Microsoft.XMLHTTP');
+        } catch (e) {}
+        try {
+            obj = new XMLHTTP('Msxml2.XMLHTTP.6.0');
+        } catch (e) {}
+        try {
+            obj = new XMLHTTP('Msxml2.XMLHTTP.3.0');
+        } catch (e) {}
+        try {
+            obj = new XMLHTTP('Msxml2.XMLHTTP');
+        } catch (e) {}
+    }
 
-  if (window.XMLHttpRequest) {
-    obj = new XMLHttpRequest();
-  }
-
-
-  return obj;
+    return obj;
 
 }
+
 var xhr = createXhr();
 console.log(xhr);
