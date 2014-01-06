9add615c7197a99f780f943cd2b5f50cdfe4631a
hidoos <sk8huaban@gmail.com>
2014-01-06 14:36:46 +0800

    add event

--- JS/event/04PropertywithEvent.js
+++ JS/event/04PropertywithEvent.js
@@ -2,12 +2,27 @@
  * @param {string} his
  * @return {Number} hisoo
  */
-var log = document.createElement('log'),
-    i = '',
-    out = '';
 
-var doc = document;
-doc.addEventListener('click',function(ev){
-    console.log('target',ev.target);
-    console.log('currentTarget',ev.currentTarget);
-},false);
+window.onload = function() {
+    var elDemo = document.querySelector('.demo')
+    i = '',
+        out = '';
+
+    var doc = document,
+        win = window;
+    doc.addEventListener('click', logeventtarget, false);
+
+    function logeventtarget(ev) {
+        var target = ev.target,
+            currentTarget = ev.currentTarget;
+        elDemo.innerHTML = 'target' + ev.target;
+        elDemo.innerHTML += '\ncurrentTarget' + ev.currentTarget;
+
+        console.log('target',ev.target);
+        console.log('currenttarget',ev.currentTarget);
+    }
+}
+
+/**
+ * target 是指触发事件的元素，ercurrentTarget属性则是指：绑定事件的元素
+ */
