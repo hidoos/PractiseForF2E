cb45f98400cdd69a3174b315079f9f9d7ffaebdd
hidoos <sk8huaban@gmail.com>
2014-01-06 15:20:20 +0800

    add mdn querySelector

--- JS/event/05delegation/05eventDelegation.js
+++ JS/event/05delegation/05eventDelegation.js
@@ -8,9 +8,31 @@
 /**
  * 下面是一个无序列表，鼠标悬停时，显示链接地址，用tagName去区分触发悬停事件的元素
  * chrome and firefox 都有 debugger，可以用来调试，可以用这个学习一下调试js
+ * 查询log返回的是一个对象
+ * https://developer.mozilla.org/en-US/docs/Web/API/document.querySelector
  */
 
 var resources = document.querySelector('#resources'),
     log = document.querySelector('.log'),
+
     i = '',
     out = '';
+
+console.log(log);
+resources.addEventListener('mouseover', function(ev) {
+    var target = ev.target;
+    switch (target.tagName) {
+        case 'A':
+            log.innerHTML = 'A link, with the href:' + target.href;
+
+            break;
+
+        case 'LI':
+            log.innerHTML = 'A list item';
+            break;
+
+        default:
+            log.innerHTML = 'The list itself';
+
+    }
+}, false);
