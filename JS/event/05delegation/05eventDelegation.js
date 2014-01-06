39e89f22d410a981afec0453000a1f4c76a94c7d
hidoos <sk8huaban@gmail.com>
2014-01-06 15:29:51 +0800

    add mouse points

--- JS/event/05delegation/05eventDelegation.js
+++ JS/event/05delegation/05eventDelegation.js
@@ -1,7 +1,7 @@
 /**
  * Event delegation
  *
- * 使用事件对象 event的target属性，可以获得当前触发事件的dom对象，这样我们只用绑定一个父级的dom元素，然后判断一下
+ * @note: 使用事件对象 event的target属性，可以获得当前触发事件的dom对象，这样我们只用绑定一个父级的dom元素，然后判断一下
  * ev.target ，想想看，如果为每一个表格去监听对象的话，那得有多麻烦啊！
  */
 
@@ -9,10 +9,12 @@
  * 下面是一个无序列表，鼠标悬停时，显示链接地址，用tagName去区分触发悬停事件的元素
  * chrome and firefox 都有 debugger，可以用来调试，可以用这个学习一下调试js
  * 查询log返回的是一个对象
- * https://developer.mozilla.org/en-US/docs/Web/API/document.querySelector
+ * @doc: https://developer.mozilla.org/en-US/docs/Web/API/document.querySelector
  */
 
 var resources = document.querySelector('#resources'),
+    /*@note: form mdn: Returns null if no matches are found; otherwise, it returns the first matching element.*/
+
     log = document.querySelector('.log'),
 
     i = '',
---
+++ JS/event/06eventPageX/eventPageX.js
@@ -0,0 +1,14 @@
+/**
+ * 判断鼠标的位置
+ * @note: a.鼠标在viewport中的位置
+ *        b. 鼠标在当前页面的位置
+ *        c. 滚动条滚动距离
+ */
+
+function getMousePoint(argument) {
+
+}
+
+/**
+ * @todo : js 操作一个小球动起来
+ */
