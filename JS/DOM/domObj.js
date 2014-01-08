087f3bf9441f4954df95543a6ec2268c077c8ced
lanhong <sk8huaban@gmail.com>
2014-01-08 21:26:59 +0800

    add jquery

--- JS/DOM/domObj.js
+++ JS/DOM/domObj.js
@@ -1,7 +1,14 @@
 /**
  * 构造函数prototype实例，this代码
+ * 文章： http://www.zhangxinxu.com/wordpress/2013/07/jquery-%E5%8E%9F%E7%90%86-%E6%9C%BA%E5%88%B6/
  */
 
 var F = function() {
 
-    }
+}
+
+/* thisiao 代表什么？ 返回什么值 */
+F.prototype.hide = function() {
+    this.style.display = "none";
+    return new F();
+}
